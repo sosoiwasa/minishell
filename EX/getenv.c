99adbe94840c59d12238f79ff798036ea4be6664
siwasa <souwasao084@gmail.com>
2023-03-09 19:35:56 +0900

    execve getenv

---
+++ EX/getenv.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getenv.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: siwasa <siwasa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 17:59:34 by siwasa            #+#    #+#             */
+/*   Updated: 2023/03/09 19:35:18 by siwasa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main()
+// {
+//     char *username = "USER";
+//     char *pwd = "PWD";
+//     char *user_value = getenv(username);
+//     char *pwd_value = getenv(pwd);
+
+//     if (user_value != NULL)
+//         printf("The current user name is %s\n", user_value);
+//     else
+//         printf("The USER environment variable does not exist.\n");
+
+//     if (pwd_value != NULL)
+//         printf("The current working directory is %s\n", pwd_value);
+//     else
+//         printf("The PWD environment variable does not exist.\n");
+
+//     return 0;
+// }
